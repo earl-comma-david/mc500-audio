a9ca0493bb503f398d82d9b5e5351455a1b67767
David Earl <davide@lunadna.com>
2023-02-28 20:50:38 -0800

    three input bytes, refactor

--- mc500-audio.cpp
+++ mc500-audio.cpp
@@ -10,99 +10,115 @@
 #include "debounce.cpp"
 #include "twi-slave.h"
 #include "twi-slave.c"
+#include "Relay.cpp"
+
+#define I2C_MESSAGE_LENGTH 3
+#define I2C_ADDR 0x10
+#define ATTENUATION_DATA_INDEX 0
+#define SWITCH_DATA_UPPER_INDEX 1
+#define SWITCH_DATA_LOWER_INDEX 2
+
+#define WORD_MASK_INPUT_1  0b1000000000000000
+#define WORD_MASK_INPUT_2  0b0100000000000000
+#define WORD_MASK_INPUT_3  0b0010000000000000
+#define WORD_MASK_OUTPUT_1 0b0000000001000000
+#define WORD_MASK_OUTPUT_2 0b0000000000100000
+#define WORD_MASK_OUTPUT_3 0b0000000000010000
+#define WORD_MASK_SUB      0b0000000000001000
+#define WORD_MASK_MONO     0b0000000000000100
+
+#define RELAY_OUT_PIN_INPUT_1  PD2
+#define RELAY_OUT_PIN_INPUT_2  PD3
+#define RELAY_OUT_PIN_INPUT_3  PD5
+#define RELAY_OUT_PIN_OUTPUT_1 PD6
+#define RELAY_OUT_PIN_OUTPUT_2 PD7
+#define RELAY_OUT_PIN_OUTPUT_3 PB0
+#define RELAY_OUT_PIN_MONO     PB1
 
 volatile uint8_t _i2cIndex;
-volatile uint8_t _data;
-volatile uint8_t _dataRight;
-
-#define I2C_ADDR 0x10
+// TODO: master reads its initial state from the slave
+volatile uint8_t _data[I2C_MESSAGE_LENGTH] = { 127, 0b10000000, 0b01000000 };
 
 void I2C_received(uint8_t received_data)
 {
-  if (_i2cIndex++ % 2 == 0)
-  {
-    _data = received_data;
-  }
-  else
-  {
-    _dataRight = received_data;
-  }
+    _data[_i2cIndex++] = received_data;
+    if (_i2cIndex == I2C_MESSAGE_LENGTH)
+    {
+        _i2cIndex = 0;
+    }
 }
 
 void I2C_requested()
 {
-  I2C_transmitByte(_data);
+  // TODO:
+  //I2C_transmitByte(_data);
+}
+
+void init_io_pins()
+{
+    // outputs
+    DDRB |= (1<<PB4);
+    DDRB |= (1<<PB3);
+    DDRB |= (1<<PB2);
+    DDRB |= (1<<PB1);
+    DDRB |= (1<<PB0);
+    DDRD |= (1<<PD7);
+    DDRD |= (1<<PD6);
+    DDRD |= (1<<PD5);
+    DDRD |= (1<<PD3);
+    DDRD |= (1<<PD2);
+}
+
+void init(void)
+{
+    I2C_setCallbacks(I2C_received, I2C_requested);
+    I2C_init(I2C_ADDR);
+
+    init_io_pins();
 }
  
 int main (void)
 {
-  DDRB = 0xFF;
-  DDRD = 0xFF;
-  PORTB = 0x00;
-  PORTD = 0x00;
+    init();
 
-  ShiftRegister shiftRegister = ShiftRegister();
-  //DDRB |= (1<<PB5);
+    uint8_t relayCount = 7;
 
-    //DDRB |= (1<<PB0);
-    //DDRD = 0xff;
+    Relay relays[relayCount] =
+    {
+        Relay(WORD_MASK_INPUT_1, &PORTD, RELAY_OUT_PIN_INPUT_1),
+        Relay(WORD_MASK_INPUT_2, &PORTD, RELAY_OUT_PIN_INPUT_2),
+        Relay(WORD_MASK_INPUT_3, &PORTD, RELAY_OUT_PIN_INPUT_3),
+        Relay(WORD_MASK_OUTPUT_1, &PORTD, RELAY_OUT_PIN_OUTPUT_1),
+        Relay(WORD_MASK_OUTPUT_2, &PORTD, RELAY_OUT_PIN_OUTPUT_2),
+        Relay(WORD_MASK_OUTPUT_3, &PORTB, RELAY_OUT_PIN_OUTPUT_3),
+        Relay(WORD_MASK_MONO, &PORTB, RELAY_OUT_PIN_MONO)
+    };
 
-    I2C_setCallbacks(I2C_received, I2C_requested);
-    I2C_init(I2C_ADDR);
-    _data = 0x00;
-
-    uint8_t lastData = 0xff;
-
+    ShiftRegister mainAttenuationShiftRegister = ShiftRegister();
+    uint8_t lastAttenuationState = 0x00;
+    uint16_t lastSwitchState = 0xff;
     uint8_t byte = 0;
+
     while(1)
     {
-        //PORTB |= 1<<PB5;
-        //_delay_ms(100);
-        //PORTB &= ~(1<<PB5);
-        //_delay_ms(50);
-        //PORTB |= 1<<PB5;
-        //_delay_ms(100);
-        //PORTB &= ~(1<<PB5);
-
-        if (_data != lastData)
+        if (_data[ATTENUATION_DATA_INDEX] != lastAttenuationState)
         {
-          shiftRegister.put(_data);
-          //shiftRegister.put(255 - byte++);
-          lastData = _data;
+            // TODO: reevaluate where the best place for this negation is:
+            lastAttenuationState = ~_data[ATTENUATION_DATA_INDEX];
+            mainAttenuationShiftRegister.put(lastAttenuationState);
         }
 
-        //PORTD |= 1<<PD2;
-        //_delay_ms(1000);
-        //PORTD &= ~(1<<PD2);
-        //_delay_ms(1000);
-        //PORTD |= 1<<PD3;
-        //_delay_ms(1000);
-        //PORTD &= ~(1<<PD3);
-        //_delay_ms(1000);
-        //PORTD |= 1<<PD5;
-        //_delay_ms(1000);
-        //PORTD &= ~(1<<PD5);
-        //_delay_ms(1000);
-        //PORTD |= 1<<PD6;
-        //_delay_ms(1000);
-        //PORTD &= ~(1<<PD6);
-        //_delay_ms(1000);
-        //PORTD |= 1<<PD7;
-        //_delay_ms(1000);
-        //PORTD &= ~(1<<PD7);
-        //_delay_ms(1000);
-        //PORTB |= 1<<PB0;
-        //_delay_ms(1000);
-        //PORTB &= ~(1<<PB0);
-        //_delay_ms(1000);
-        //PORTB |= 1<<PB1;
-        //_delay_ms(1000);
-        //PORTB &= ~(1<<PB1);
-        //_delay_ms(1000);
+        uint16_t data = (_data[SWITCH_DATA_UPPER_INDEX] << 8) | _data[SWITCH_DATA_LOWER_INDEX];
 
-        //PORTD = _data;
+        if (data != lastSwitchState)
+        {
+            lastSwitchState = data;
 
-        //_delay_ms(100);
+            for (int i = 0; i < relayCount; i++)
+            {
+                relays[i].scan(data);
+            }
+        }
     }
 
     return 0;
